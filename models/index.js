2da27768510003a06c0c4f299d6898028d146d91
Temitope-Emmanuel <temitopeojo0@gmail.com>
2020-01-05 17:51:15 -0800

    v2

--- models/index.js
+++ models/index.js
@@ -2,11 +2,21 @@ const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
 
-mongoose.connect( process.env.MONGOLAB_URI, {
-  useNewUrlParser:true,
-  keepAlive: true,
-  useUnifiedTopology: true
-});
+const uri = "mongodb+srv://Temitope:drowssap@warblercluster-jsojs.mongodb.net/test?retryWrites=true&w=majority"
+
+mongoose.connect(uri);
+
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://<username>:<password>@warblercluster-jsojs.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
 
 
 // const MongoClient = require('mongodb').MongoClient;
