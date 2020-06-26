d93d503b24a98acf45e2e2d71dd271f198fc6863
Temitope-Emmanuel <temitopeojo0@gmail.com>
2020-06-26 05:12:38 +0100

    deleted sensitive info

--- models/index.js
+++ models/index.js
@@ -2,33 +2,13 @@ const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
 
-// const uri = "mongodb+srv://Temitope:drowssap@warblercluster-jsojs.mongodb.net/test?retryWrites=true&w=majority"
-
 mongoose.connect(process.env.MONGOLAB_URI);
 
 
 
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://<username>:<password>@warblercluster-jsojs.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
 
 
 
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://Temitope:drowssap@warblercluster-jsojs.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { 
-//   useNewUrlParser: true,useUnifiedTopology:true 
-//  });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
 
 
 
